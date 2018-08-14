d2a77beeb96603b4fa68482e1877a5e6066375e8
Caio Zullo <caiozullo@gmail.com>
2018-08-14 20:01:51 +0100

    Break test into two tests

--- WeakRef/WeakRefTests/WeakRefTests.swift
+++ WeakRef/WeakRefTests/WeakRefTests.swift
@@ -7,14 +7,20 @@ import WeakRef
 
 class WeakRefTests: XCTestCase {
 	
+	func testWeakRefHoldsAReferenceToTheGivenObject() {
+		let anObject = MockObject()
+		
+		let sut = WeakRef(anObject)
+		
+		XCTAssertEqual(ObjectIdentifier(sut.object!), ObjectIdentifier(anObject))
+	}
+	
 	func testWeakRefHoldsAWeakReferenceToTheGivenObject() {
 		var anObject: MockObject? = MockObject() // anObject retain count = 1
 		
 		let sut = WeakRef(anObject!)
 		
-		XCTAssertEqual(ObjectIdentifier(sut.object!), ObjectIdentifier(anObject!), "Object reference should not be nil while it's still in memory")
-		
-		anObject = nil // retain count = 0 (memory should be freed now)
+		anObject = nil // anObject retain count = 0 (memory should be freed now)
 		
 		XCTAssertNil(sut.object, "Object reference should be nil after the object has been freed")
 	}
