f1d16144a43e5ef5612b6a1b5548d1d1c20ba3f9
Caio Zullo <caiozullo@gmail.com>
2018-08-14 19:38:53 +0100

    Create WeakRef framework project

---
+++ WeakRef/WeakRef/WeakRef.h
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeakRef.
+FOUNDATION_EXPORT double WeakRefVersionNumber;
+
+//! Project version string for WeakRef.
+FOUNDATION_EXPORT const unsigned char WeakRefVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeakRef/PublicHeader.h>
---
+++ WeakRef/WeakRefTests/WeakRefTests.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import WeakRef
+
+class WeakRefTests: XCTestCase {
+	
+}
