d78b919a403e3a2138e57aefaab9dc1da9ec450d
Caio Zullo <caiozullo@gmail.com>
2018-08-14 19:44:24 +0100

    Add `WeakRef` type to WeakRef framework project

--- WeakRef/WeakRefTests/WeakRefTests.swift
+++ WeakRef/WeakRefTests/WeakRefTests.swift
@@ -3,8 +3,21 @@
 //
 
 import XCTest
-import WeakRef
+@testable import WeakRef
 
 class WeakRefTests: XCTestCase {
 	
+	func testWeakRefHoldsAWeakReferenceToTheGivenObject() {
+		var anObject: MockObject? = MockObject() // anObject retain count = 1
+		
+		let sut = WeakRef(anObject!)
+		
+		XCTAssertEqual(ObjectIdentifier(sut.object!), ObjectIdentifier(anObject!), "Object reference should not be nil while it's still in memory")
+		
+		anObject = nil // retain count = 0 (memory should be freed now)
+		
+		XCTAssertNil(sut.object, "Object reference should be nil after the object has been freed")
+	}
+	
+	private class MockObject {}
 }
